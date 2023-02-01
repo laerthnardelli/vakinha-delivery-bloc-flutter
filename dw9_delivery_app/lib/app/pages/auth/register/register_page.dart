cf8182b159b1a4df1a98c226feeb6493d8fa93ec
laerthnardelli <laerthnardelli@gmail.com>
2023-02-01 20:30:23 -0300

    add design RegisterPage

--- dw9_delivery_app/lib/app/pages/auth/register/register_page.dart
+++ dw9_delivery_app/lib/app/pages/auth/register/register_page.dart
@@ -1,4 +1,8 @@
+import 'package:dw9_delivery_app/app/core/ui/styles/text_styles.dart';
+import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_appbar.dart';
 import 'package:flutter/material.dart';
+
+import '../../../core/ui/widgets/delivery_button.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
@@ -6,10 +10,52 @@ class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
+      appBar: DeliveryAppbar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Cadastro',
+                  style: context.textStyles.textTitle,
+                ),
+                Text(
+                  'Preencha os campos abaixo para criar o seu cadastro.',
+                  style: context.textStyles.textMedium.copyWith(fontSize: 18),
+                ),
+                const SizedBox(height: 30),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Nome'),
+                ),
+                const SizedBox(height: 30),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'E-mail'),
+                ),
+                const SizedBox(height: 30),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Senha'),
+                ),
+                const SizedBox(height: 30),
+                TextFormField(
+                  decoration:
+                      const InputDecoration(labelText: 'Confirma senha'),
+                ),
+                const SizedBox(height: 30),
+                Center(
+                  child: DeliveryButton(
+                    width: double.infinity,
+                    label: 'CADASTRAR',
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-      body: Container(),
     );
   }
 }
