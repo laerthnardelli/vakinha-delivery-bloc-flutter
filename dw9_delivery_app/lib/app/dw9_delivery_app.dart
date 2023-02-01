37f0a6f3fad3be984e6cb370aa81f4e1fb1bab55
laerthnardelli <laerthnardelli@gmail.com>
2023-02-01 20:23:02 -0300

    add RegisterPage and route

--- dw9_delivery_app/lib/app/dw9_delivery_app.dart
+++ dw9_delivery_app/lib/app/dw9_delivery_app.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 
 import 'core/provider/application_binding.dart';
 import 'pages/auth/login/login_page.dart';
+import 'pages/auth/register/register_page.dart';
 import 'pages/splash/splash_page.dart';
 
 class Dw9DeliveryApp extends StatelessWidget {
@@ -21,6 +22,7 @@ class Dw9DeliveryApp extends StatelessWidget {
           '/home': (context) => HomeRouter.page,
           '/productDetail': (context) => ProductDetailRouter.page,
           '/auth/login': (context) => const LoginPage(),
+          '/auth/register': (context) => const RegisterPage(),
         },
       ),
     );
