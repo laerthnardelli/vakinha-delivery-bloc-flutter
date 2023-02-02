e3066e99034c006c3cee4e5c7aa0bfe521ab4a18
laerthnardelli <laerthnardelli@gmail.com>
2023-02-02 16:09:24 -0300

    integration LoginPage

--- dw9_delivery_app/lib/app/dw9_delivery_app.dart
+++ dw9_delivery_app/lib/app/dw9_delivery_app.dart
@@ -4,8 +4,7 @@ import 'package:dw9_delivery_app/app/pages/product_detail/product_detail_router.
 import 'package:flutter/material.dart';
 
 import 'core/provider/application_binding.dart';
-import 'pages/auth/login/login_page.dart';
-import 'pages/auth/register/register_page.dart';
+import 'pages/auth/login/login_router.dart';
 import 'pages/auth/register/register_router.dart';
 import 'pages/splash/splash_page.dart';
 
@@ -22,7 +21,7 @@ class Dw9DeliveryApp extends StatelessWidget {
           '/': (context) => const SplashPage(),
           '/home': (context) => HomeRouter.page,
           '/productDetail': (context) => ProductDetailRouter.page,
-          '/auth/login': (context) => const LoginPage(),
+          '/auth/login': (context) => LoginRouter.page,
           '/auth/register': (context) => RegisterRouter.page,
         },
       ),
