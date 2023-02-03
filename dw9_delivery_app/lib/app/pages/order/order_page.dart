0b9343ad3a6070e727984e5e8a4fb39bb94698a8
laerthnardelli <laerthnardelli@gmail.com>
2023-02-03 11:34:29 -0300

    add design to OrderPage

--- dw9_delivery_app/lib/app/pages/order/order_page.dart
+++ dw9_delivery_app/lib/app/pages/order/order_page.dart
@@ -1,4 +1,14 @@
+import 'package:dw9_delivery_app/app/core/ui/styles/text_styles.dart';
+import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_appbar.dart';
+import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_button.dart';
+import 'package:dw9_delivery_app/app/dto/order_product_dto.dart';
+import 'package:dw9_delivery_app/app/models/product_model.dart';
 import 'package:flutter/material.dart';
+import 'package:validatorless/validatorless.dart';
+
+import 'widget/order_field.dart';
+import 'widget/order_product_tile.dart';
+import 'widget/payment_types_field.dart';
 
 class OrderPage extends StatelessWidget {
   const OrderPage({super.key});
@@ -6,10 +16,107 @@ class OrderPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
+      appBar: DeliveryAppbar(),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Row(
+                children: [
+                  Text(
+                    'Carrinho',
+                    style: context.textStyles.textTitle,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Image.asset(
+                      'assets/images/trashRegular.png',
+                      color: Colors.red,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              childCount: 10,
+              (context, index) {
+                return Column(
+                  children: [
+                    OrderProductTile(
+                      index: index,
+                      orderProduct: OrderProductDto(
+                        product: ProductModel.fromMap({}),
+                        amount: 10,
+                      ),
+                    ),
+                    const Divider(color: Colors.grey),
+                  ],
+                );
+              },
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Total do pedido',
+                        style: context.textStyles.textExtraBold
+                            .copyWith(fontSize: 16),
+                      ),
+                      Text(
+                        r'R$ 200,00',
+                        style: context.textStyles.textExtraBold
+                            .copyWith(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+                const Divider(color: Colors.grey),
+                OrderField(
+                  title: 'Endereço de Entrega',
+                  controller: TextEditingController(),
+                  validator: Validatorless.required('m'),
+                  hintText: 'Digite um endereço',
+                ),
+                const SizedBox(height: 10),
+                OrderField(
+                  title: 'CPF',
+                  controller: TextEditingController(),
+                  validator: Validatorless.required('m'),
+                  hintText: 'Digite o CPF',
+                ),
+                const PaymentTypesField(),
+              ],
+            ),
+          ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Divider(color: Colors.grey),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: DeliveryButton(
+                    width: double.infinity,
+                    height: 48,
+                    label: 'FINALIZAR',
+                    onPressed: () {},
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
-      body: Container(),
     );
   }
 }
